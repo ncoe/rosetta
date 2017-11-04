7003545a5a6fdced15479bafed3116e43e1c24cb
Nathan Coe <robbiec1987@yahoo.com>
2017-11-03 19:58:10 -0600

    Added solution for the cartesian product problem

---
+++ Cartesian_product_of_two_or_more_lists/source/app.d
@@ -0,0 +1,40 @@
+//https://rosettacode.org/wiki/Cartesian_product_of_two_or_more_lists
+import std.stdio;
+
+void main() {
+    auto a = listProduct([1,2], [3,4]);
+    writeln(a);
+
+    auto b = listProduct([3,4], [1,2]);
+    writeln(b);
+
+    auto c = listProduct([1,2], []);
+    writeln(c);
+
+    auto d = listProduct([], [1,2]);
+    writeln(d);
+}
+
+auto listProduct(T)(T[] ta, T[] tb) {
+    struct Result {
+        int i, j;
+
+        bool empty() {
+            return i>=ta.length
+                || j>=tb.length;
+        }
+
+        T[] front() {
+            return [ta[i], tb[j]];
+        }
+
+        void popFront() {
+            if (++j>=tb.length) {
+                j=0;
+                i++;
+            }
+        }
+    }
+
+    return Result();
+}
