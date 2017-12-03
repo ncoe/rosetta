a30ba8e821b00a0892abe649e5dbe990ebc27f50
Nathan Coe <robbiec1987@yahoo.com>
2017-12-03 12:18:09 -0700

    Added solution to the fork problem

---
+++ Fork/source/app.d
@@ -0,0 +1,10 @@
+//https://rosettacode.org/wiki/Fork
+import core.thread;
+import std.stdio;
+
+void main() {
+    new Thread({
+        writeln("Spawned thread.");
+    }).start;
+    writeln("Main thread.");
+}
