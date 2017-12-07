92b7258d8a03191fee909dbdf6257f1fd6cedf6a
Nathan Coe <robbiec1987@yahoo.com>
2017-12-06 18:21:07 -0700

    Added solution to the write to windows event log problem

---
+++ Write_to_Windows_event_log/source/app.d
@@ -0,0 +1,13 @@
+//https://rosettacode.org/wiki/Write_to_Windows_event_log
+import std.process;
+import std.stdio;
+
+void main() {
+    auto cmd = executeShell(`EventCreate /t INFORMATION /id 123 /l APPLICATION /so Dlang /d "Rosetta Code Example"`);
+
+    if (cmd.status == 0) {
+        writeln("Output: ", cmd.output);
+    } else {
+        writeln("Failed to execute command, status=", cmd.status);
+    }
+}
