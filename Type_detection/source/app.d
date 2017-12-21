f4ccd7a674d7a743ae355297061fc20775f48aa4
Nathan Coe <robbiec1987@yahoo.com>
2017-12-20 19:07:33 -0700

    Added solution to the type detection problem

---
+++ Type_detection/source/app.d
@@ -0,0 +1,21 @@
+//https://rosettacode.org/wiki/Type_detection
+import std.stdio;
+
+auto typeString(T)(T _) {
+    return T.stringof;
+}
+
+class C {}
+struct S {}
+
+void main() {
+    writeln(typeString(1));
+    writeln(typeString(1L));
+    writeln(typeString(1.0f));
+    writeln(typeString(1.0));
+    writeln(typeString('c'));
+    writeln(typeString("string"));
+    writeln(typeString(new C()));
+    writeln(typeString(S()));
+    writeln(typeString(null));
+}
