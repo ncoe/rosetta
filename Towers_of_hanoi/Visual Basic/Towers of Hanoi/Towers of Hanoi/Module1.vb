48d6bd471502f95700aec131855e145385d4274b
Nathan Coe <robbiec1987@yahoo.com>
2019-02-06 17:36:18 -0700

    added solution to the towers of hanoi problem using visual basic

---
+++ Towers_of_hanoi/Visual Basic/Towers of Hanoi/Towers of Hanoi/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Move(n As Integer, fromPeg As Integer, toPeg As Integer, viaPeg As Integer)
+        If n > 0 Then
+            Move(n - 1, fromPeg, viaPeg, toPeg)
+            Console.WriteLine("Move disk from {0} to {1}", fromPeg, toPeg)
+            Move(n - 1, viaPeg, toPeg, fromPeg)
+        End If
+    End Sub
+
+    Sub Main()
+        Move(4, 1, 2, 3)
+        Console.WriteLine("Puzzle completed!")
+        Console.ReadLine()
+    End Sub
+
+End Module
