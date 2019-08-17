4d2480895f983ee62b2dd435fe139df5080b4fdc
Nathan Coe <robbiec1987@yahoo.com>
2019-08-16 19:56:40 -0600

    Added solution to the largest number divisible by its digits problem using c#

---
+++ Largest_number_divisible_by_its_digits/CS/LargestNumber/LargestNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LargestNumber {
+    class Program {
+        static bool ChkDec(int num) {
+            HashSet<int> set = new HashSet<int>();
+
+            return num.ToString()
+                .Select(c => c - '0')
+                .All(d => (d != 0) && (num % d == 0) && set.Add(d));
+        }
+
+        static void Main() {
+            int result = Enumerable.Range(0, 98764321)
+                .Reverse()
+                .Where(ChkDec)
+                .First();
+            Console.WriteLine(result);
+        }
+    }
+}
