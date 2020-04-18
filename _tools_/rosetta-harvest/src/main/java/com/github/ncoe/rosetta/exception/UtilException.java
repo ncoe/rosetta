f2af1b2a096d791d2251ca655661d111657e289f
Nathan Coe <robbiec1987@yahoo.com>
2020-04-18 10:47:11 -0600

    Added solution to the print debugging statement problem using d

--- _tools_/rosetta-harvest/src/main/java/com/github/ncoe/rosetta/exception/UtilException.java
+++ _tools_/rosetta-harvest/src/main/java/com/github/ncoe/rosetta/exception/UtilException.java
@@ -4,13 +4,15 @@ package com.github.ncoe.rosetta.exception;
  * For avoiding having to annotate every method with the exceptions it throws.
  */
 public class UtilException extends RuntimeException {
+    private static final long serialVersionUID = -7344926881296347627L;
+
     /**
-     * Constructs a new runtime exception with the specified detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
+     * Constructs a new runtime exception with the specified detail message. The
+     * cause is not initialized, and may subsequently be initialized by a call to
+     * {@link #initCause}.
      *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
+     * @param message the detail message. The detail message is saved for later
+     *                retrieval by the {@link #getMessage()} method.
      */
     public UtilException(String message) {
         super(message);
